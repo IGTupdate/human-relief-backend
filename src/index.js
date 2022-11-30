239bc6731292930e0f2f34eeb165c8f2c122f4fe
Kuldeep Sen <kuldeepsen134@gmail.com>
2022-11-30 19:15:07 +0530

    Fixed postID unique issue done

--- src/index.js
+++ src/index.js
@@ -9,26 +9,26 @@ import paypalRouter from './routes/paypalRouter.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+// const URL = 'mongodb+srv://kuldeepsen:KKss1997@cluster0.v2fxl4e.mongodb.net/?retryWrites=true&w=majority';
 const URL = 'mongodb+srv://react1:YEWO4P4C8eEffOCm@cluster0.rxk8i.mongodb.net/Cluster0?retryWrites=true&w=majority';
 
 
-
 app.use(cors());
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/api', router); 
-app.use('/', paypalRouter); 
- 
-app.use(express.static('public')); 
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', router);
+app.use('/', paypalRouter);
+
+app.use(express.static('public'));
 app.use('/uploads', express.static('images'));
 
- 
- 
-mongoose.connect(URL,{
-    useNewUrlParser: true, 
-    useUnifiedTopology:true
-}).then(()=>{
-    app.listen(PORT,()=> console.log(`Server is running on port http://localhost:${PORT}`));
-}).catch((error)=>{
+
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
+}).catch((error) => {
     console.log(`Error${error}`);
 });
--- src/model/wishlistScema.js
+++ src/model/wishlistScema.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const wishlistScema = mongoose.Schema({
-    postId:String,
-    userId:String,
-    dateofCreation:String,
+    postId: { type: String, unique: true },
+    userId: String,
+    dateofCreation: String,
 });
- 
-const Wishlist = mongoose.model('wishlist',wishlistScema);
+
+const Wishlist = mongoose.model('wishlist', wishlistScema);
 
 export default Wishlist; 
